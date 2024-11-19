5859690045675f11b539ca5fd1bcc4b22edf1142
Mahdi Moghadam <yamahdico@yahoo.com>
2024-11-19 16:32:31 +0330

    Create basic.go

---
+++ basic.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+var cc, python, java bool
+
+func main() {
+	var a bool = true    // Boolean
+	var b int = 5        // Integer
+	var c float32 = 3.14 // Floating point number
+	var d string = "Hi!" // String
+
+	fmt.Println("Boolean: ", a)
+	fmt.Println("Integer: ", b)
+	fmt.Println("Float:   ", c)
+	fmt.Println("String:  ", d)
+
+}
